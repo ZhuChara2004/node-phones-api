7fc47b4a73620975f35371cc4e671967611594fe
Dmitry Zhuk <zhuchara@gmail.com>
2018-10-09 13:34:54 +0300

    delete phone action added

--- routes/phonesRouter.mjs
+++ routes/phonesRouter.mjs
@@ -41,4 +41,17 @@ export default function phonesRouter(app) {
       }
     });
   });
+
+  app.delete('/phones/:id', (req, res) => {
+    Phone.findById(req.params.id, (err, doc) => {
+      if (!doc) {
+        res.status(404).json({ status: 'Not found' })
+      } else {
+        doc.remove((err) => {
+          if (err) return res.status(500).json({ message: err.message });
+          res.status(200).json(doc)
+        });
+      }
+    });
+  });
 }
